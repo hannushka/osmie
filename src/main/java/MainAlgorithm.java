4085890870e1640d8898cc71dc935a99b01b63c0
Hannah Lindblad <lindbladhannah@gmail.com>
2018-03-01 13:14:31 +0100

    Initial commit

---
+++ src/main/java/MainAlgorithm.java
@@ -0,0 +1,11 @@
+
+public class MainAlgorithm {
+
+    public void run() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        new MainAlgorithm().run();
+    }
+}
