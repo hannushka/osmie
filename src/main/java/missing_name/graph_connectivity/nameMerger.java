dad77cdbd7d38f4a8387ceb613d9956fda59aaa1
Hampus Londögård <hampus.londogard.866@student.lu.se>
2018-03-08 15:03:43 +0100

    Added more heuristics to the nameMerger.

--- src/main/java/missing_name/graph_connectivity/nameMerger.java
+++ src/main/java/missing_name/graph_connectivity/nameMerger.java
@@ -4,6 +4,7 @@ import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.geography.atlas.AtlasResourceLoader;
 import org.openstreetmap.atlas.geography.atlas.items.Edge;
 import org.openstreetmap.atlas.streaming.resource.File;
+import org.openstreetmap.atlas.tags.HighwayTag;
 import org.openstreetmap.atlas.tags.names.NameTag;
 
 import java.util.HashSet;
@@ -14,7 +15,7 @@ import java.util.stream.Collectors;
 
 
 public class nameMerger {
-    private static String ATLAS_FILE = "data/atlas/POINT (8.194954 55.8843184).atlas";
+    private static String ATLAS_FILE = "data/atlas/POINT.atlas";
 
     private static String smallTailCase(Edge edge){   // Also check noname-tag.
         Set<Edge> connectedEdges = edge.connectedEdges();
@@ -33,8 +34,8 @@ public class nameMerger {
     private static String inBetweenNamedEdges(Edge edge){      // Starting with ONLY the case of MASVAGEN -> __ -> MASVAGEN
         Set<Edge> inEdges = edge.inEdges();
         Set<Edge> outEdges = edge.outEdges();
-        inEdges.removeIf(it -> it.isReversedEdge(edge));
-        outEdges.removeIf(it -> it.isReversedEdge(edge));
+        inEdges.removeIf(it -> it.isReversedEdge(edge) || it.connectedEdges().stream().anyMatch(inEdges::contains));
+        outEdges.removeIf(it -> it.isReversedEdge(edge) || it.connectedEdges().stream().anyMatch(inEdges::contains));
         Set<String> inNames = inEdges.stream()
                 .map(it -> it.getTag(NameTag.KEY).orElse(""))
                 .collect(Collectors.toSet());
@@ -43,22 +44,53 @@ public class nameMerger {
                 .map(it -> it.getTag(NameTag.KEY).orElse(""))
                 .collect(Collectors.toSet());
         outNames.remove("");
-        if(inNames.size() == 1 && outNames.size() == 1 && inNames.toArray()[0].equals(outNames.toArray()[0]))
+        if(inNames.size() == 1 && outNames.size() == 1
+                && inNames.toArray()[0].equals(outNames.toArray()[0]))
             return (String) inNames.toArray()[0];
         else return "";
     }
 
-    public static String generateNameSuggestion(Edge edge){
+    private static String changeOfPathType(Edge edge){
+        Set<Edge> connectedEdges = edge.connectedEdges();
+        String highwayType = edge.getTag(HighwayTag.KEY).orElse("");
+        if(highwayType.isEmpty()) return "";
+
+        if(connectedEdges.stream().anyMatch(it -> {
+            String itType = it.getTag(HighwayTag.KEY).orElse("");
+            return !itType.isEmpty() && itType.equals(highwayType);
+        })){
+            return "Add noname=true tag";
+        }
+        return "";
+    }
+
+    public static String isRoundAbout(Edge edge){
+        if(edge.getTag("junction").orElse("").equals("roundabout")) return "Add noname=true tag";
+        return "";
+    }
+
+    public static String generateNameSuggestion(Edge edge){     //	motorway_link --> name of motorway
         String name = edge.getTag(NameTag.KEY).orElse("");
         String noname = edge.getTag("noname").orElse("");
         if(name.isEmpty() && noname.isEmpty()){
-            String smallTail = smallTailCase(edge);
-            if(!smallTail.isEmpty()) return smallTail;
+            String changeOfPathType = changeOfPathType(edge);
+            if(!changeOfPathType.isEmpty()) return changeOfPathType;
 
-            System.out.println("Going for inBet");
+            String roundabout = isRoundAbout(edge);
+            if(!roundabout.isEmpty()) return roundabout;
+
+            String smallTail = smallTailCase(edge);
+            if(!smallTail.isEmpty()){
+                System.out.println("smallTail found!");
+                return smallTail;
+            }
+
+
             String inBetween = inBetweenNamedEdges(edge);
-            if(!inBetween.isEmpty()) return inBetween;
-            System.out.println("Fuck.");
+            if(!inBetween.isEmpty()){
+                System.out.println("inBetween found!");
+                return inBetween;
+            }
 
             Set<Edge> inEdges = edge.inEdges();
             Set<Edge> outEdges = edge.outEdges();
@@ -76,7 +108,7 @@ public class nameMerger {
         Atlas atlas = new AtlasResourceLoader().load(new File(ATLAS_FILE));
         for(Edge edge: atlas.edges()){
             String suggestion = generateNameSuggestion(edge);
-            System.out.println("Suggestion: " + suggestion);
+            System.out.println("Suggestion: " + suggestion + " (" + edge.getMasterEdgeIdentifier() + ")");
         }
     }
 }
