4475eedb56344d25860afc27be6bcc42b92d7c3a
Lundez <Lundez>
2018-04-10 14:56:09 +0200

    Keeping Runner as BRNN

--- src/main/java/neural_nets/DeepLearningTester.java
+++ src/main/java/neural_nets/DeepLearningTester.java
@@ -2,20 +2,21 @@ package neural_nets;
 
 
 import neural_nets.anomalies.AnomaliesRNN;
+import neural_nets.spellchecker.BiDirectionalRNN;
 
 import java.util.Scanner;
 
 public class DeepLearningTester {
     public static void main(String[] args) {
-        String fileLocation = "data/dataAnomalies.csv";
-        String testFileLocation = "data/dataAnomaliesTest.csv";
-        String modelFilePathPrefix = "data/models/ARNN_";
+        String fileLocation = "data/shuffledTrainData.csv";
+        String testFileLocation = "data/manualNameData.csv";
+        String modelFilePathPrefix = "data/models/BRNN_";
         Seq2Seq model;
         try {
             Scanner keyboard = new Scanner(System.in);
-            for(int i = 0; i < 1; i += 10){
-                model = AnomaliesRNN.Builder()
-                        .setCharacterIterator(fileLocation, testFileLocation, Seq2Seq.IteratorType.ANOMALIES, false)
+            for(int i = 10; i < 500; i += 10){
+                model = BiDirectionalRNN.Builder()
+                        .setCharacterIterator(fileLocation, testFileLocation, Seq2Seq.IteratorType.CLASSIC, false)
                         .loadModel(String.format("%s%s.bin", modelFilePathPrefix, i));
                 model.runTesting(false);
                 System.out.println("Nr:" + i);
